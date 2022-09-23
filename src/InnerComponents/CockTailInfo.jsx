1e5f89b31ba2d035a84560f5e00bbff9b1b98fa8
Steve724 <yan.1439@osu.edu>
2022-09-22 23:59:40 -0400

    add click cocktail name function

--- src/InnerComponents/CockTailInfo.jsx
+++ src/InnerComponents/CockTailInfo.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./cockTail.css"
+import {setCocktailArr,selectCocktailArr} from "../redux/features/cocktailArr/cocktailArrSlice";
+import {useDispatch,useSelector} from "react-redux";
+
 export default function CockTailInfo({strDrink,strDrinkThumb,strIngredient1,strIngredient2,strIngredient3,strIngredient4}){
+    const dispatch = useDispatch();
+    function handleClick(){
+        dispatch(setCocktailArr(strDrink));
+    }
     return (
         <div style={{borderWidth:"1px",borderRadius:"20px",borderColor:"black",borderStyle:"solid",marginLeft:"200px",marginRight:"800px"}}>
             <img src={strDrinkThumb} style={{width:"150px",height:"120px",display:"inline-block",paddingTop:"5px",paddingLeft:"5px"}}/>
-            <p style={{display:"inline-block",float:"right"}}>{strDrink}</p>
+            <p style={{display:"inline-block",float:"right"}} onClick={()=>{
+                handleClick();
+            }}>{strDrink}</p>
             <h5 className="item" style={{color:"black",margin:"3px",paddingLeft:"10px"}}>Ingredient:</h5>
             <p className="item">{strIngredient1}</p>
             <p className="item">{strIngredient2}</p>
